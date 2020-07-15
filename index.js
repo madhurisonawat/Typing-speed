710d6e86ff69fcd66defbd149414d2b7da331ba8
lakshmikorukanti <luckyrao4e8@gmail.com>
2020-07-16 01:14:46 +0530

    modified

--- index.js
+++ index.js
@@ -30,47 +30,63 @@ let quotes_array3 = [
   "Once there was a crow who wishes to be colorful and beautiful like other birds. He then went to the parrot and shared his thoughts.But parrot said peacock is most beautiful bird so talk to him. Then the crow went to the peacock and told him about his looks.Then the peacock replied,” You are the luckiest bird that has been never caged in life and we because of our beauty stay caged, and you are always free”.After listening to this, crow realized his mistake and thanked God for making him like this and he flew away happily. ",
   "Once there was a Lion in the jungle who used to kill 2-3 animals daily for his meal. All animals went to him to tell, that daily one of them will come to him for his meal.So, the Lion agreed and this started going for many days. One day, it was Rabbit’s turn. When he was on his way he saw a well.Now he plans to kill the lion and save himself. He went to the lion and told him that, there is another lion who claims to be more powerful than him.Then the lion asks the rabbit to take him to that lion. The rabbit takes him to the well and said he lives here. When the lion looked in the well he saw his own reflection and jumped in the well and dies.",
 ];
-
-let quote1 = document.getElementById("quote1");
-let error_text1 = document.querySelector("currCal_error1");
-let input_area1 = document.getElementById("textArea1");
-let timer_next1 = document.getElementById("currCal_time1");
-let accuracy_text1 = document.getElementById("currCal_accu1");
-let wpm_text1 = document.getElementById("currCal_wpm1");
-let accuracy_group = document.querySelectorAll(".accuracy1");
-let input_char1 = 0;
-let error1 = 0;
+let TIME_LIMIT1 = 60;
+let timer_text1 = document.querySelector(".currCal_time1");
+let acc_text1 = document.querySelector(".currCal_accu1");
+let err_text1 = document.querySelector(".currCal_error1");
+let wpm_text1 = document.querySelector(".currCal_wpm1");
+let quote_text1 = document.querySelector("#quote1");
+let input_area1 = document.querySelector("#textArea1");
+let restart_btn = document.querySelector("#restart_btn");
+let wpm_grp1 = document.querySelector(".wpm1");
+let err_grp1 = document.querySelector(".errors1");
+let acc_grp1 = document.querySelector(".accuracy1");
+let start1 = document.getElementById("reset1");
+let time_left1 = TIME_LIMIT1;
+let time_elapsed1 = 0;
 let total_errors1 = 0;
+let errors1 = 0;
+let accuracy1 = 0;
+let characterTyped1 = 0;
 let current_quote1 = "";
+let quoteNo1 = 0;
 let timer1 = null;
-function startGame() {
-  updateQuote();
-}
+start1.addEventListener("click", startGame);
+
 function updateQuote() {
-  const quoteNo1 = Math.floor(Math.random() * quotes_array1.length);
-  console.log(quoteNo);
+  quote_text1.textContent = null;
+  quoteNo1 = Math.floor(Math.random() * quotes_array1.length);
   current_quote1 = quotes_array1[quoteNo1];
-  console.log(current_quote);
   current_quote1.split("").forEach((char) => {
     const charSpan1 = document.createElement("span");
     charSpan1.innerText = char;
-    quote1.appendChild(charSpan);
+    quote_text1.appendChild(charSpan1);
   });
+
+  // roll over to the first quote
+  if (quoteNo1 < quotes_array1.length - 1) quoteNo1++;
+  else quoteNo1 = 0;
 }
-function checkInput() {
-  let arr1 = [];
-  let quote1_text = input_area1.value;
-  arr = quote1_text.split("");
-  input_char1++;
-  error1 = 0;
-  quoteSpanArray1 = quote1.querySelectorAll("span");
+
+function processCurrentText() {
+  // get current input text and split it
+  curr_input1 = input_area1.value;
+  curr_input1_array = curr_input1.split("");
+
+  // increment total characters typed
+  characterTyped1++;
+
+  errors1 = 0;
+
+  quoteSpanArray1 = quote_text1.querySelectorAll("span");
   quoteSpanArray1.forEach((char, index) => {
-    let typedChar1 = arr1[index];
-    console.log(typedChar);
+    let typedChar1 = curr_input1_array[index];
+
+    // characters not currently typed
     if (typedChar1 == null) {
       char.classList.remove("correct_char");
       char.classList.remove("incorrect_char");
-    } else if (typedChar === char.innerText) {
+    } else if (typedChar1 === char.innerText) {
       char.classList.add("correct_char");
       char.classList.remove("incorrect_char");
     } else {
@@ -79,18 +95,73 @@ function checkInput() {
       errors1++;
     }
   });
-  error_text1.textContent = total_errors + errors;
+
+  err_text1.textContent = total_errors1 + errors1;
   let correctCharacters1 = characterTyped1 - (total_errors1 + errors1);
-  let accuracyVal1 = (correctCharacters1 / characterTyped1) * 100;
-  accuracy_text1.textContent = Math.round(accuracyVal1);
-  if (quote1_text.length == current_quote1.length) {
+  let accuracy1Val = (correctCharacters1 / characterTyped1) * 100;
+  acc_text1.textContent = Math.round(accuracy1Val);
+  if (curr_input1.length == current_quote1.length) {
     updateQuote();
     total_errors1 += errors1;
     input_area1.value = "";
   }
 }
 
-window.addEventListener("load", function () {
-  let start1 = document.getElementById("reset1");
-  start1.addEventListener("click", startGame);
-});
+function updateTimer() {
+  if (time_left1 > 0) {
+    // decrease the current time left
+    time_left1--;
+
+    // increase the time elapsed
+    time_elapsed1++;
+
+    // update the timer1 text
+    timer_text1.textContent = time_left1 + "s";
+  } else {
+    // finish the game
+    finishGame();
+  }
+}
+
+function finishGame() {
+  // stop the timer1
+  clearInterval(timer1);
+
+  // disable the input area
+  input_area1.disabled = true;
+
+  // show finishing text
+  quote_text1.textContent = "Click on restart to start a new game.";
+  restart_btn.style.display = "block";
+
+  let wpm = Math.round((characterTyped1 / 5 / time_elapsed1) * 60);
+  console.log(wpm);
+  wpm_text1.textContent = wpm;
+
+  wpm_grp1.style.display = "block";
+}
+
+function startGame() {
+  resetValues();
+  updateQuote();
+  clearInterval(timer1);
+  timer1 = setInterval(updateTimer, 1000);
+}
+
+function resetValues() {
+  time_left1 = TIME_LIMIT1;
+  time_elapsed1 = 0;
+  errors1 = 0;
+  total_errors11 = 0;
+  accuracy1 = 0;
+  characterTyped1 = 0;
+  quoteNo1 = 0;
+  input_area1.disabled = false;
+
+  input_area1.value = "";
+
+  acc_text1.textContent = 100;
+  timer_text1.textContent = time_left1 + "s";
+  err_text1.textContent = 0;
+  wpm_grp1.style.display = "none";
+}
